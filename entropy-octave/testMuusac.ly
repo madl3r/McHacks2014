557b273223d639fea17e177d8449ab3777b3d2d5
Matthew Adler <madler72@gmail.com>
2014-02-22 21:23:59 -0500

    Conver to lily simple completed

---
+++ entropy-octave/testMuusac.ly
@@ -0,0 +1,2 @@
+\header{title = "testMuusac"} {\time 4/4 
+ \clef bass a4 b2 c8 d16 }
--- entropy-octave/toLily.m
+++ entropy-octave/toLily.m
@@ -1,19 +1,29 @@
 function score = gen_notes(filename)
 
 score = []
-score = [score "{\\time 4/4 \n \\clef bass"]
+score = [score "\\header{title = \"" filename "\"} {\\time 4/4 \n \\clef bass "]
 
-theFile = fopen(filename)
+fopen(filename)
 
-theMatrix = dlmread(filename)
+[notes, beats] = textread(filename, '%s %s')
 
-for i = 1:size(theMatrix)(1)
-	score = [score theMatrix(i, 1) theMatrix(i, 2) ' ']
+for i = 1:size(notes)(1)
+	%currentNote = substr(notes{i}, 2, size(notes{i}) - 2)
+	currentNote = regexprep(notes{i}, "\"", "")
+	score = [score currentNote beats{i} ' ']
 end
 
 score = [score '}']
 
-fwrite([filename '.ly'], score)
+fclose(filename)
+
+theFile = fopen([filename '.ly'], 'w')
+
+fprintf(theFile, '%s', score)
+
+
+
+fclose(theFile)
 
 end
 
