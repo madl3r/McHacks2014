5e1a9688b3b51399f171c640dcf2a0cea03321ac
hck32 <hck32@cornell.edu>
2014-02-22 21:15:12 -0500

    producing proper score format

--- entropy-octave/gen_diatonic_rthm1_wioct.m
+++ entropy-octave/gen_diatonic_rthm1_wioct.m
@@ -2,17 +2,23 @@ function gen_diatonic_rthm1_wioct(npitches, bpm, filename)
   bps = 16;
   sps = 8000;
   
-  piano = dlmread('piano.csv');
+  [name1,name2,piano] = textread('fullPiano.csv','%s %s %f');
+
 
   wave  = [];
-  freqs = [];
+  notes = {};
   lens  = [];
   key   = 43;
   for i = 1:npitches
     keyo  = unidrnd(25)-13;
     key   = max(min((key + keyo),66),20);
     freq  = piano(key);
-    freqs = [freqs; freq];
+    if (rand(1)>0.5)
+      note = name1{key};
+    else
+      note = name2{key};
+    endif
+    notes{i} = note;
     b     = (binornd(4,0.25));
     nsecs = (2^(b-2))*60/bpm;
     lens  = [lens; 2^(4-b)]; %writes lilypond-style note length
@@ -21,6 +27,14 @@ function gen_diatonic_rthm1_wioct(npitches, bpm, filename)
 
   wave = wave';
 
-  dlmwrite([filename '.score'], [freqs, lens], ',');
+  %dlmwrite([filename '.score'], [notes, lens], ',');
+  fid = fopen([filename '.score'],'w');
+  for i = 1:npitches
+    fprintf(fid,'%s\n', [notes{i} ',' int2str(lens(i))]);
+  end
+  
+  fclose(fid);
+
+
   wavwrite(wave, sps, bps, [filename '.wav']);
 
